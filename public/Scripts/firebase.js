5a8ab2f41a695a391be5822460e527d1e2ee7156
alonestreak <sushantsawant865@gmail.com>
2021-08-21 20:32:33 +0530

    Done with deployment on firebase

---
+++ public/Scripts/firebase.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB1sAcbUCDhz-HOxNAVQr8iD--VGUY8LtQ",
+    authDomain: "q-blog-website-3a248.firebaseapp.com",
+    projectId: "q-blog-website-3a248",
+    storageBucket: "q-blog-website-3a248.appspot.com",
+    messagingSenderId: "713710488843",
+    appId: "1:713710488843:web:e2937d59ea6f9460c6260e",
+    measurementId: "G-ZYEK1K1VXE"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
